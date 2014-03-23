0cedfe2060ab5a84955bd7fcf475f8c3282d5fab
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2014-03-23 20:11:09 +0100

    cleanup includes

--- main.c
+++ main.c
@@ -4,9 +4,8 @@
  */
 #include <errno.h>
 #include <fcntl.h>
-#include <stdlib.h>
 #include <stdio.h>
-#include <stdint.h>
+#include <stdlib.h>
 #include <string.h>
 #include <termios.h>
 #include <unistd.h>
